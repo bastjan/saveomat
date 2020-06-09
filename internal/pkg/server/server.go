56086261c9a716870a06052190d26f6967e0c308
xermicus <bigcyrill@hotmail.com>
2020-06-09 16:25:37 +0200

    helm repo and chart download

--- internal/pkg/server/server.go
+++ internal/pkg/server/server.go
@@ -3,17 +3,25 @@ package server
 import (
 	"bufio"
 	"context"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/bastjan/saveomat/internal/pkg/auth"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"github.com/gofrs/flock"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/mikefarah/yq/v3/pkg/yqlib"
 	"github.com/pkg/errors"
+	"gopkg.in/yaml.v3"
+	"helm.sh/helm/v3/pkg/chart"
 	"helm.sh/helm/v3/pkg/cli"
 	"helm.sh/helm/v3/pkg/downloader"
 	"helm.sh/helm/v3/pkg/getter"
@@ -21,9 +29,13 @@ import (
 )
 
 var (
-	repoConfig  = os.Getenv("HELM_REPO_CONFIG_FILE")
-	repoCache   = os.Getenv("HELM_REPO_DIR")
+	repoFile    = os.Getenv("HELM_REPO_CONFIG_FILE")
+	repoCache   = os.Getenv("HELM_REPO_CACHE_DIR")
 	downloadDir = os.Getenv("HELM_DOWNLOAD_DIR")
+	getters     = getter.All(&cli.EnvSettings{
+		RepositoryConfig: repoFile,
+		RepositoryCache:  repoCache,
+	})
 )
 
 type ServerOpts struct {
@@ -37,6 +49,11 @@ type Server struct {
 }
 
 func NewServer(opt ServerOpts) *Server {
+	//err := os.MkdirAll(filepath.Dir(repoFile), os.ModePerm)
+	//if err != nil && !os.IsExist(err) {
+	//	return err
+	//}
+
 	e := echo.New()
 	s := &Server{e, opt.DockerClient}
 
@@ -51,7 +68,8 @@ func NewServer(opt ServerOpts) *Server {
 	})
 	g.POST("/tar", s.postTar)
 	g.GET("/tar", s.getTar)
-	g.POST("/helm", s.postHelm)
+	g.POST("/helm/repo", s.postHelmRepo)
+	g.POST("/helm/chart", s.postHelmChart)
 
 	return s
 }
@@ -94,49 +112,124 @@ func (s *Server) postTar(c echo.Context) error {
 	return s.streamImages(c, authn, normalizeImages(images))
 }
 
-func (s *Server) postHelm(c echo.Context) error {
+func (s *Server) postHelmRepo(c echo.Context) error {
+	name := c.FormValue("name")
+	url := c.FormValue("url")
+	if name == "" {
+		return fmt.Errorf("Please specify a repository name using the name form param")
+	}
+	if url == "" {
+		return fmt.Errorf("Please specify a repository URL using the url form param")
+	}
+
+	// The following code is mostly plugged from https://github.com/helm/helm/blob/master/cmd/helm/repo_add.go#L85
+	fileLock := flock.New(strings.Replace(repoFile, filepath.Ext(repoFile), ".lock", 1))
+	lockCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	locked, err := fileLock.TryLockContext(lockCtx, time.Second)
+	if err == nil && locked {
+		defer fileLock.Unlock()
+	}
+	if err != nil {
+		return err
+	}
+
+	b, err := ioutil.ReadFile(repoFile)
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+
+	var f repo.File
+	if err := yaml.Unmarshal(b, &f); err != nil {
+		return err
+	}
+
+	cfg := repo.Entry{
+		Name:                  name,
+		URL:                   url,
+		Username:              c.FormValue("username"),
+		Password:              c.FormValue("password"),
+		InsecureSkipTLSverify: false,
+	}
+	repo, err := repo.NewChartRepository(&cfg, getters)
+	if err != nil {
+		return err
+	}
+	if repoCache != "" {
+		repo.CachePath = repoCache
+	}
+
+	if _, err := repo.DownloadIndexFile(); err != nil {
+		return errors.Wrapf(err, "looks like %q is not a valid chart repository or cannot be reached", url)
+	}
+
+	f.Update(&cfg)
+	if err := f.WriteFile(repoFile, 0644); err != nil {
+		return err
+	}
+
+	c.Logger().Info(fmt.Sprintf("Repository addedd successfully: %s %s", name, url))
+	return nil
+}
+
+func (s *Server) postHelmChart(c echo.Context) error {
 	_, err := c.FormFile("values.yaml")
 	if err != nil {
 		return err
 	}
 
-	repoName := c.FormValue("repoName")
-	repoURL := c.FormValue("repoURL")
-	repoCfg := repo.Entry{
-		Name: repoName,
-		URL:  repoURL,
-	}
-	repo, err := repo.NewChartRepository(&repoCfg, getter.All(&cli.EnvSettings{
-		RepositoryConfig: repoConfig,
-		RepositoryCache:  repoCache,
-	}))
-	if err != nil {
-		return err
-	}
-	if _, err := repo.DownloadIndexFile(); err != nil {
-		return errors.Wrapf(err, "looks like %q is not a valid chart repository or cannot be reached", repoURL)
-	}
-
 	dl := downloader.ChartDownloader{
 		Out:              os.Stderr,
-		RepositoryConfig: repoConfig,
+		RepositoryConfig: repoFile,
 		RepositoryCache:  repoCache,
-		Getters: getter.All(&cli.EnvSettings{
-			RepositoryConfig: repoConfig,
-			RepositoryCache:  repoCache,
-		}),
+		Getters:          getters,
 	}
 	chrtRef := c.FormValue("chart")
 	chrtVer := c.FormValue("version")
-	destFile, _, err := dl.DownloadTo(chrtRef, chrtVer, "/tmp/dl")
+	destFile, _, err := dl.DownloadTo(chrtRef, chrtVer, downloadDir)
 	if err != nil {
 		return err
 	}
 
 	c.Logger().Info(destFile)
+	return nil
 
-	var images []string
-	return s.streamImages(c, auth.EmptyAuthenticator, normalizeImages(images))
+	//var images []string
+	//return s.streamImages(c, auth.EmptyAuthenticator, normalizeImages(images))
+}
+
+func findImagesInHelmChart(chrt *chart.Chart, values map[string]interface{}) ([]string, error) {
+	return nil, nil
+	//rendered, err := engine.New().Render(chrt, values)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//var images = make([]string, len(rendered))
+	//for filename, contents := range rendered {
+	//	if !strings.HasSuffix(filename, "yaml") && !strings.HasSuffix(filename, "yml") {
+	//		continue
+	//	}
+	//	nodes, err := findImageNodeInYaml(contents)
+	//	if err != nil {
+	//		return nil, err
+	//	}
+	//	images = append(images, nodes...)
+	//}
+	//return images, nil
+}
+
+func findImageNodeInYaml(yml string) ([]string, error) {
+	var node yaml.Node
+	err := yaml.Unmarshal([]byte(yml), &node)
+	nodes, err := yqlib.NewYqLib().Get(&node, "**.image", false)
+	if err != nil {
+		return nil, err
+	}
+	var result []string
+	for _, match := range nodes {
+		result = append(result, fmt.Sprintf("%s", match.Node.Value))
+	}
+	return result, nil
 }
 
 func normalizeImages(images []string) []string {
